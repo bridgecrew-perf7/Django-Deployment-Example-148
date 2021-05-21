1dc48831d41b564e4476b40367ddaeb63f137b2d
tryhardlaijun <73064145+tryhardlaijun@users.noreply.github.com>
2021-05-21 23:14:31 +0800

    Update settings.py

--- Practice 5/LearningUser/LearningUser/settings.py
+++ Practice 5/LearningUser/LearningUser/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+TEMPLATE_DIR = os.path.join(BASE_DIR, "Templates")
 STATIC_DIR = os.path.join(BASE_DIR, "static")
 MEDIA_DIR = os.path.join(BASE_DIR,"media")
 
